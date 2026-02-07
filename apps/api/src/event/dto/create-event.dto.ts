fd72da2578791f52c52eebeaf3bcf32669235fe8
soaud arziki <arziki.souad@gmail.com>
2026-02-07 11:24:15 +0100

    AEB-18 la creation du module du schema d'un event, et des dtos de creation et modification (CreateEventDto, UpdateEventDto)

---
+++ apps/api/src/event/dto/create-event.dto.ts
@@ -0,0 +1,19 @@
+import { IsDateString, IsInt, IsString, Min } from 'class-validator';
+
+export class CreateEventDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  description: string;
+
+  @IsDateString()
+  date: string;
+
+  @IsString()
+  location: string;
+
+  @IsInt()
+  @Min(1)
+  capacity: number;
+}
